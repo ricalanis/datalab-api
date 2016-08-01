82726f847d83fdbe186152b9f38d679d17499422
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-07-31 21:32:02 -0500

    adds strings

--- tools.py
+++ tools.py
@@ -1,24 +1,30 @@
 
 def get_places(list_points):
-    places = []
-    for point in list_points:
-        places.append(point["geopoint"]["coordinates"])
+    try:
+        places = []
+        for point in list_points:
+            places.append(point["geopoint"]["coordinates"])
+    except:
+        places = "NaN"
     return places
 
 
 def get_traces(list_places):
-    routes = []
-    for place in list_places:
-        local_steps = "["
-        i = 0
-        for route in place["route"]:
-            if i > 0:
-                initial_string = ","
-            else:
-                initial_string = ""
-            local_steps.append(initial_string + "["+route["end_location"]["lng"]+","+route["end_location"]["lat"]+"]")
-        local_steps = local_steps + "]"
-        routes.append(local_steps)
+    try:
+        routes = []
+        for place in list_places:
+            local_steps = "["
+            i = 0
+            for route in place["route"]:
+                if i > 0:
+                    initial_string = ","
+                else:
+                    initial_string = ""
+                local_steps.append(initial_string + "["+route["end_location"]["lng"]+","+route["end_location"]["lat"]+"]")
+            local_steps = local_steps + "]"
+            routes.append(local_steps)
+    except:
+        routes = "NaN"
     return routes
 
 
