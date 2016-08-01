c07b872f296cbeeb0d9f5bec5ff2b1a616ec2e57
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-07-31 22:03:51 -0500

    Add stringifier

--- tools.py
+++ tools.py
@@ -13,16 +13,19 @@ def get_traces(list_places):
     try:
         routes = []
         for place in list_places:
-            local_steps = "["
             i = 0
+            local_steps = ""
             for route in place["route"]:
                 if i > 0:
                     initial_string = ","
                 else:
-                    initial_string = ""
-                local_steps.append(initial_string + "["+route["end_location"]["lng"]+","+route["end_location"]["lat"]+"]")
+                    initial_string = "["
+                local_steps = local_steps + initial_string + "["+str(route["end_location"]["lng"])+","+str(route["end_location"]["lat"])+"]"
+                i = i + 1
             local_steps = local_steps + "]"
+            print(local_steps)
             routes.append(local_steps)
+            print(local_steps)
     except:
         routes = "NaN"
     return routes
